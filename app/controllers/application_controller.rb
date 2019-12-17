8d1fa459fde48abed8ced1dac82a52ed2067b7f7
bbbbruno <bbbbruno69@gmail.com>
2019-12-18 01:09:52 +0900

    add column username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
@@ -19,4 +20,10 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(user)
     new_user_session_path
   end
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    end
 end
