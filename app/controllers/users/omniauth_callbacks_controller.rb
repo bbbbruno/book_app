760d9c91621f0fc07ae8b11b38763e0562a5344c
bbbbruno <bbbbruno69@gmail.com>
2019-12-22 01:20:56 +0900

    add activestorage

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,13 +2,15 @@
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    @user = User.find_for_github_oauth(request.env['omniauth.auth'], current_user)
+    auth = request.env['omniauth.auth']
+    @user = User.find_for_github_oauth(auth, current_user)
 
     if @user.persisted?
+      @user.set_avatar(auth)
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
     else
-      session['devise.github_data'] = request.env['omniauth.auth']
+      session['devise.github_data'] = auth
       redirect_to new_user_registration_url
     end
 
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,6 +3,24 @@
 class Profile < ApplicationRecord
   belongs_to :user
 
+  has_one_attached :avatar
+  before_create { self.avatar.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default_avatar.png')), filename: 'dafault_avatar.png', content_type: 'image/png') }
+
+  scope :recent, -> { order(created_at: :desc) }
+
   validates :user_id, uniqueness: true
-  validates :zipcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }, on: :update
+  validates :zipcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, allow_blank: true }, on: :update
+
+  def profile_avatar(size)
+    resize =
+      case size
+      when :sm
+        [50, 50]
+      when :md
+        [200, 200]
+      when :lg
+        [300, 300]
+      end
+    self.avatar.variant(resize_to_fill: resize)
+  end
 end
